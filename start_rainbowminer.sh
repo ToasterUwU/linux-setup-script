dc36efb007e053e100b8514126f18ec0ee2684eb
ToasterUwU <aki@toasteruwu.com>
2023-04-16 19:06:21 +0200

    formatting

--- start_rainbowminer.sh
+++ start_rainbowminer.sh
@@ -1,6 +1,10 @@
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 gsettings set org.gnome.desktop.screensaver lock-enabled false
-cd /home/aki/RainbowMiner 
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+
+cd /home/aki/RainbowMiner
 /home/aki/RainbowMiner/start.sh
+
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled true
 gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim true
