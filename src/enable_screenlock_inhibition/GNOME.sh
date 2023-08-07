3847015633bc5d968b92319aec2ab6f98a8433d1
ToasterUwU <aki@toasteruwu.com>
2023-08-08 00:07:26 +0200

    reorder

--- src/enable_screenlock_inhibition/GNOME.sh
+++ src/enable_screenlock_inhibition/GNOME.sh
@@ -1,10 +1,10 @@
-stop_inhibiting() {
-    /bin/kill "$INHIBIT_PID"
-}
-
 trap stop_inhibiting EXIT
 
 inhibitcmd="gnome-session-inhibit --inhibit idle --inhibit-only sleep infinity"
 $inhibitcmd >/dev/null 2>&1 &
 
 INHIBIT_PID=$!
+
+stop_inhibiting() {
+    /bin/kill "$INHIBIT_PID"
+}
