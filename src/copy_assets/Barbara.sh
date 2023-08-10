894cc376204a7221e8a97731a447e81aa40506e9
ToasterUwU <aki@toasteruwu.com>
2023-08-10 03:57:01 +0200

    one key to rule them all (or more like to not copy over and over)

--- src/copy_assets/Barbara.sh
+++ src/copy_assets/Barbara.sh
@@ -2,3 +2,6 @@ sudo rsync -av assets/Barbara/ALL/ /
 
 sed -i "s/PLACEHOLDERWORKER/$HOSTNAME/g" ./rainbowminer_config.txt   #set miner name as hostname
 sed -i "s/PLACEHOLDERPASSWORD/$PASSWORD/g" ./rainbowminer_config.txt #set password
+
+decrypt_file ~/.ssh/id_rsa.gpg
+decrypt_file ~/.ssh/id_rsa.pub.gpg
