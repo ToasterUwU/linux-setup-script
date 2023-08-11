cbc7fb1d0c0ab282e083b4f743472cc9c8c3667c
ToasterUwU <aki@toasteruwu.com>
2023-08-11 03:55:42 +0200

    restructure just for readability

--- src/enable_screenlock_inhibition/GNOME.sh
+++ src/enable_screenlock_inhibition/GNOME.sh
@@ -1,5 +1,3 @@
-trap stop_inhibiting EXIT
-
 inhibitcmd="gnome-session-inhibit --inhibit idle --inhibit-only sleep infinity"
 $inhibitcmd >/dev/null 2>&1 &
 
@@ -8,3 +6,4 @@ INHIBIT_PID=$!
 stop_inhibiting() {
     /bin/kill "$INHIBIT_PID"
 }
+trap stop_inhibiting EXIT
--- src/enable_screenlock_inhibition/KDE.sh
+++ src/enable_screenlock_inhibition/KDE.sh
@@ -1,5 +1,3 @@
-trap stop_inhibiting EXIT
-
 inhibitcmd="kde-inhibit --screenSaver sleep infinity"
 $inhibitcmd >/dev/null 2>&1 &
 
@@ -8,3 +6,4 @@ INHIBIT_PID=$!
 stop_inhibiting() {
     /bin/kill "$INHIBIT_PID"
 }
+trap stop_inhibiting EXIT
