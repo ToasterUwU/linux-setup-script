32c6b0e017afe6cc0ac6796541e897408754fad7
ToasterUwU <aki@toasteruwu.com>
2023-08-12 18:58:42 +0200

    simplefying "open in Code" dolphin extension install

--- src/coding_setup/KDE.sh
+++
@@ -1,3 +0,0 @@
-echo "Adding 'Open in Code' Entry to Filemanager"
-wget "https://raw.githubusercontent.com/Merrit/kde-dolphin-open-vscode/master/openVSCode.desktop" -O ~/.local/share/kservices5/openVSCode.desktop
-echo ""
