f5af8b8a1d89dc7753acf516c4e95dd476ef3eaf
ToasterUwU <aki@toasteruwu.com>
2023-08-23 23:12:54 +0200

    make sure permissions for ssh files are correct

--- src/copy_assets/Gertrude.sh
+++ src/copy_assets/Gertrude.sh
@@ -2,4 +2,8 @@ sudo rsync -rltv $CWD/assets/Gertrude/ALL/ /
 
 decrypt_file ~/.ssh/id_rsa.gpg
 decrypt_file ~/.ssh/id_rsa.pub.gpg
+sudo chown aki ~/.ssh/id_rsa
+sudo chown aki ~/.ssh/id_rsa.pub
+sudo chmod 600 ~/.ssh/id_rsa
+sudo chmod 600 ~/.ssh/id_rsa.pub
 ssh-add
