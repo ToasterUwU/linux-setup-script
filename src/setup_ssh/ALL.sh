c87b0273af6465dd7da798a97fdf35918b471446
ToasterUwU <aki@toasteruwu.com>
2023-08-23 23:01:37 +0200

    make sure permissions arent a problem

--- src/setup_ssh/ALL.sh
+++ src/setup_ssh/ALL.sh
@@ -18,5 +18,5 @@ echo ""
 echo "Copying SSH Key to Gutruhn (My NAS)"
 sudo apt install sshpass -y #can put ssh passwords in automatically (stdin doesnt work because of openssh design choices)
 
-sshpass -p "$PASSWORD" ssh-copy-id Aki@toasteruwu.com #this is my NAS, which i connect to via sshfs (SFTP with Fuse)
+sshpass -p "$PASSWORD" sudo ssh-copy-id Aki@toasteruwu.com #this is my NAS, which i connect to via sshfs (SFTP with Fuse)
 echo ""
