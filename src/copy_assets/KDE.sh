ac0f3859b5f694456a15cdcec8513421fffe6d17
ToasterUwU <aki@toasteruwu.com>
2023-09-04 20:56:48 +0200

    feat(assets): add new plasmoid file applet-wunderground-pws.plasmoid feat(copy_assets): add installation and removal of plasmoid in KDE.sh script

--- src/copy_assets/KDE.sh
+++ src/copy_assets/KDE.sh
@@ -3,3 +3,5 @@ copy_assets ALL/KDE
 sed -i "s/PLACEHOLDERHOSTNAME/$HOSTNAME/g" /home/aki/.config/kuprc
 
 decrypt_file ~/start_microsoft_rewards_bot.sh.gpg
+
+sudo plasmapkg2 -i ~/applet-wunderground-pws.plasmoid && rm -f ~/applet-wunderground-pws.plasmoid
