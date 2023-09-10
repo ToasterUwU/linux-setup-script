f439457862fbe5c89782bc11bb7e301f1f34a3f9
ToasterUwU <aki@toasteruwu.com>
2023-09-10 15:20:24 +0200

    refactor(KDE.sh): replace echo and tee commands with kwriteconfig5 for setting NumLock

--- src/configure_de/KDE.sh
+++ src/configure_de/KDE.sh
@@ -1,7 +1,3 @@
 echo "Setting Numlock to be on by default"
-NUMLOCK_SETTING="
-[Keyboard]
-NumLock=0
-"
-echo "$NUMLOCK_SETTING" | sudo tee -a ~/.config/kcminputrc >/dev/null # add sshfs entries (my NAS shares)
+kwriteconfig5 --file ~/.config/kcminputrc --group Keyboard --key NumLock "0"
 echo ""
