4ebdc299d663528e85ae423a7443f01dc03afeb3
ToasterUwU <aki@toasteruwu.com>
2023-10-17 23:31:21 +0200

    fix: add missing flag

--- src/install_software/Barbara.sh
+++ src/install_software/Barbara.sh
@@ -5,5 +5,5 @@ echo ""
 echo "Installing Packages and Flatpaks"
 sudo apt install handbrake-cli handbrake -y
 flatpak install -y org.prismlauncher.PrismLauncher com.vysp3r.ProtonPlus
-pip install protonup
+pip install protonup --break-system-packages
 echo ""
