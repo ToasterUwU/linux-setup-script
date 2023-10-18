a80228f82fa913700987a1fb2d92c050a44bc7fd
ToasterUwU <aki@toasteruwu.com>
2023-10-18 18:39:30 +0200

    feat(copy_assets/ALL.sh): add command to create Code directory on Desktop

--- src/copy_assets/ALL.sh
+++ src/copy_assets/ALL.sh
@@ -3,3 +3,5 @@ copy_assets ALL/ALL
 decrypt_file ~/microsoft_rewards_bot_accounts.json.gpg
 
 sed -i "s/PLACEHOLDERHOSTNAME/$HOSTNAME/g" ~/Tdarr/configs/Tdarr_Node_Config.json
+
+mkdir ~/Desktop/Code
