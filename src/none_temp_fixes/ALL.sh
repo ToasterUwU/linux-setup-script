45f493915f1afa483dd5a4db997b09ba8949efdf
ToasterUwU <aki@toasteruwu.com>
2023-10-18 18:34:30 +0200

    fix(none_temp_fixes): add automatic confirmation for libfuse2 installation feat(none_temp_fixes): add orchis-theme-gtk installation to ALL.sh feat(none_temp_fixes): add orchis-theme-kde installation to KDE.sh

--- src/none_temp_fixes/ALL.sh
+++ src/none_temp_fixes/ALL.sh
@@ -3,5 +3,9 @@ sudo apt install v4l2loopback-dkms -y
 echo ""
 
 echo "Install libfuse to be able to use AppImages"
-sudo apt install libfuse2
+sudo apt install libfuse2 -y
+echo ""
+
+echo "Install Theme that i like manually so it doesnt get removed"
+sudo apt install orchis-theme-gtk -y
 echo ""
--- src/none_temp_fixes/KDE.sh
+++ src/none_temp_fixes/KDE.sh
@@ -1,3 +1,7 @@
 echo "Installing missing Dep for KRDC on Wayland"
 sudo apt install freerdp2-wayland -y
 echo ""
+
+echo "Install Theme that i like manually so it doesnt get removed"
+sudo apt install orchis-theme-kde -y
+echo ""
