6aa3ab35764df872f74af0704cee24a632c53e99
ToasterUwU <aki@toasteruwu.com>
2023-10-19 19:56:57 +0200

    refactor: No more auto login on Barbara with Gnome

--- src/none_temp_fixes/Barbara_GNOME.sh
+++
@@ -1,4 +0,0 @@
-echo "We are on Barbara, activating auto login"
-sudo sed -i "s/#  AutomaticLoginEnable = true/AutomaticLoginEnable = true/g" /etc/gdm3/custom.conf
-sudo sed -i "s/#  AutomaticLogin = user1/AutomaticLogin = aki/g" /etc/gdm3/custom.conf
-echo ""
