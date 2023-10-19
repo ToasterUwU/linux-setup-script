99fd8e97dc0508080ac11f6b24962eabfaa0066a
ToasterUwU <aki@toasteruwu.com>
2023-10-19 20:24:01 +0200

    fix(ALL.sh): add cifs-utils installation command fix(ALL.sh): remove username from SMB entries

--- src/extend_fstab/ALL.sh
+++ src/extend_fstab/ALL.sh
@@ -1,3 +1,7 @@
+echo "Installing cifs-utils for using SMB"
+sudo apt install cifs-utils -y
+echo ""
+
 echo "Adding SMB Entries"
 sudo mkdir /mnt/home
 sudo mkdir /mnt/data
@@ -7,10 +11,10 @@ sudo mkdir /mnt/web
 SMB_ENTRIES="
 # SMB connections to Gutruhn
 
-//Aki@192.168.178.3/home /mnt/home cifs credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
-//Aki@192.168.178.3/data /mnt/data cifs credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
-//Aki@192.168.178.3/backups /mnt/backups cifs credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
-//Aki@192.168.178.3/web /mnt/web cifs credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
+//192.168.178.3/home /mnt/home cifs credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
+//192.168.178.3/data /mnt/data cifs credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
+//192.168.178.3/backups /mnt/backups cifs credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
+//192.168.178.3/web /mnt/web cifs credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
 "
 
 echo "$SMB_ENTRIES" | sudo tee -a /etc/fstab >/dev/null # add sshfs entries (my NAS shares)
