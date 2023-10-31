6d0351113f66d4ff535db8861d8f6d104b43392f
ToasterUwU <aki@toasteruwu.com>
2023-10-31 16:27:47 +0100

    feat(install_software/ALL.sh): add com.ultimaker.cura to flatpak installations feat(temp_fixes/ALL.sh): add fix for Cura not starting due to QT_STYLE_OVERRIDE environment variable

--- src/temp_fixes/ALL.sh
+++ src/temp_fixes/ALL.sh
@@ -2,3 +2,7 @@ echo "Temp Fix for OpenJDK 20 not installing because of ca-certificates-java not
 sudo apt install openjdk-19-jre-headless openjdk-19-jre -y
 sudo apt install ca-certificates-java -y
 echo ""
+
+echo "Fix Cura not starting because of QT_STYLE_OVERRIDE being set"
+sudo flatpak override --env=QT_STYLE_OVERRIDE="" com.ultimaker.cura
+echo ""
