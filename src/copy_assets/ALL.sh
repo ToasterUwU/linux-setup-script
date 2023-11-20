d04e62d686d12e2390e46b0735d2517d5889176d
ToasterUwU <aki@toasteruwu.com>
2023-11-20 19:23:09 +0100

    Revert "make sure i own my cura settings"
    This reverts commit cc83cee1c87733948ae572cf29e998ebea926ed3.

--- src/copy_assets/ALL.sh
+++ src/copy_assets/ALL.sh
@@ -8,6 +8,3 @@ sudo chmod 0600 /home/aki/.smb
 sed -i "s/PLACEHOLDERHOSTNAME/$HOSTNAME/g" ~/Tdarr/configs/Tdarr_Node_Config.json
 
 mkdir ~/Desktop/Code
-
-sudo chown aki:aki ~/.var/app/com.ultimaker.cura/config/cura/* -R
-sudo chmod 0700 ~/.var/app/com.ultimaker.cura/config/cura/* -R
