37fde57a56102f1439e2f7889cd39d1d29e06ac4
ToasterUwU <aki@toasteruwu.com>
2023-12-02 15:08:09 +0100

    refactor(install scripts): move OpenRGB installation to main software install
    Thats because i made a OpenRGB PikaOS package myself now c:

--- src/install_software/Barbara.sh
+++ src/install_software/Barbara.sh
@@ -3,7 +3,7 @@ sudo apt install -y pika-gameutils-meta #install welcome apps
 echo ""
 
 echo "Installing Packages and Flatpaks"
-sudo apt install handbrake-cli handbrake -y
+sudo apt install handbrake-cli handbrake openrgb -y
 flatpak install -y org.prismlauncher.PrismLauncher com.vysp3r.ProtonPlus
 pip install protonup --break-system-packages
 echo ""
