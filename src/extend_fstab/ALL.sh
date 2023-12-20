4bb98fbfd032018438fb55ae229b9990d15384e4
ToasterUwU <aki@toasteruwu.com>
2023-12-20 13:25:51 +0100

    Revert "refactor(extend_fstab): Adjust file and directory"
    This reverts commit 9aa7d2d81d8dcfd3fa1b322ee6194eaf9ec2ca1e.

--- src/extend_fstab/ALL.sh
+++ src/extend_fstab/ALL.sh
@@ -11,10 +11,10 @@ sudo mkdir /mnt/web
 SMB_ENTRIES="
 # SMB connections to Gutruhn
 
-//192.168.178.3/home /mnt/home cifs x-gvfs-show,credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0777,file_mode=0777 0 0
-//192.168.178.3/data /mnt/data cifs x-gvfs-show,credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0777,file_mode=0777 0 0
-//192.168.178.3/backups /mnt/backups cifs x-gvfs-show,credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0777,file_mode=0777 0 0
-//192.168.178.3/web /mnt/web cifs x-gvfs-show,credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0777,file_mode=0777 0 0
+//192.168.178.3/home /mnt/home cifs x-gvfs-show,credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
+//192.168.178.3/data /mnt/data cifs x-gvfs-show,credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
+//192.168.178.3/backups /mnt/backups cifs x-gvfs-show,credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
+//192.168.178.3/web /mnt/web cifs x-gvfs-show,credentials=/home/aki/.smb,iocharset=utf8,uid=1000,gid=1000,dir_mode=0755,file_mode=0644 0 0
 "
 
 echo "$SMB_ENTRIES" | sudo tee -a /etc/fstab >/dev/null # add sshfs entries (my NAS shares)
