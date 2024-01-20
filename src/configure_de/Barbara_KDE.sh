bfa730e1cedf67fb37c6ed1958c8c73a00ec4026
ToasterUwU <aki@toasteruwu.com>
2024-01-21 00:03:36 +0100

    fixed a var name

--- src/configure_de/Barbara_KDE.sh
+++ src/configure_de/Barbara_KDE.sh
@@ -1,7 +1,7 @@
 echo "Disabling Mouse Acceleration"
-NUMLOCK_SETTING="
+ACCELERATION_SETTING="
 [Libinput][1133][49970][Logitech Gaming Mouse G502]
 PointerAccelerationProfile=1
 "
-echo "$NUMLOCK_SETTING" | sudo tee -a ~/.config/kcminputrc >/dev/null
+echo "$ACCELERATION_SETTING" | sudo tee -a ~/.config/kcminputrc >/dev/null
 echo ""
