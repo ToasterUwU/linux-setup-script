eaa8dc2da65494d9a1256ef0b4db497d62be5fc0
ToasterUwU <aki@toasteruwu.com>
2024-01-21 14:17:29 +0100

    Change folder color on KDE

--- src/configure_de/KDE.sh
+++ src/configure_de/KDE.sh
@@ -1,3 +1,8 @@
 echo "Setting Numlock to be on by default"
 kwriteconfig5 --file ~/.config/kcminputrc --group Keyboard --key NumLock "0"
 echo ""
+
+echo "Setting Folder color of Papirus"
+wget -qO- https://git.io/papirus-folders-install | sh
+papirus-folders -u -C magenta
+echo ""
