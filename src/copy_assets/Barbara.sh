cd95596d34775794fc17bc65b39e337f259261d1
ToasterUwU <aki@toasteruwu.com>
2024-02-14 18:03:13 +0100

    proper ssh file permissions

--- src/copy_assets/Barbara.sh
+++ src/copy_assets/Barbara.sh
@@ -8,8 +8,13 @@ sed -i "s/PLACEHOLDERPASSWORD/$PASSWORD/g" ./rainbowminer_config.txt #set passwo
 decrypt_file ~/.ssh/id_rsa.gpg
 decrypt_file ~/.ssh/id_rsa.pub.gpg
 sudo chown aki:aki ~/.ssh/ -R
-sudo chmod 0600 ~/.ssh/ -R
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/known_hosts
+chmod 600 ~/.ssh/config
 ssh-add
 
 sudo chown aki:aki ~/.config/unity3d/Misfits Attic/Duskers/* -R
-sudo chmod 0700 ~/.config/unity3d/Misfits Attic/Duskers/* -R
+sudo chmod 700 ~/.config/unity3d/Misfits Attic/Duskers/* -R
--- src/copy_assets/Gertrude.sh
+++ src/copy_assets/Gertrude.sh
@@ -5,5 +5,10 @@ copy_assets Gertrude/ALL
 decrypt_file ~/.ssh/id_rsa.gpg
 decrypt_file ~/.ssh/id_rsa.pub.gpg
 sudo chown aki:aki ~/.ssh/ -R
-sudo chmod 0600 ~/.ssh/ -R
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/known_hosts
+chmod 600 ~/.ssh/config
 ssh-add
