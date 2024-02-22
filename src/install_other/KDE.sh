5928b89a1751aa76c27347e3422726dd21186aa6
ToasterUwU <aki@toasteruwu.com>
2024-02-22 11:46:43 +0100

    Fixed conflicting packages by letting them overwrite each other (dangerous, but ok in this case)

--- src/install_other/KDE.sh
+++ src/install_other/KDE.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Install Wallpaper Engine KDE Plugin"
-sudo apt install -y build-essential libvulkan-dev plasma-workspace-dev gstreamer1.0-libav liblz4-dev libmpv-dev python3-websockets qtbase5-private-dev libqt5x11extras5-dev qml-module-qtwebchannel qml-module-qtwebsockets cmake vulkan-sdk
+sudo apt -o Dpkg::Options::="--force-overwrite" install -y build-essential libvulkan-dev plasma-workspace-dev gstreamer1.0-libav liblz4-dev libmpv-dev python3-websockets qtbase5-private-dev libqt5x11extras5-dev qml-module-qtwebchannel qml-module-qtwebsockets cmake vulkan-sdk
 
 # Download source
 git clone https://github.com/catsout/wallpaper-engine-kde-plugin.git
@@ -24,8 +24,4 @@ make -j$nproc
 make install_pkg
 # install lib
 sudo make install
-echo ""
-
-echo "Temp fix, because of package conflict"
-sudo apt -y remove vulkan-sdk
 echo ""
