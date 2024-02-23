fe546223c3e9c05dbe7d80e66e5ee65a00146799
ToasterUwU <Aki@ToasterUwU.com>
2024-02-23 12:01:41 +0100

    That update manager doesnt exist anymore

--- src/none_temp_fixes/ALL.sh
+++ src/none_temp_fixes/ALL.sh
@@ -8,10 +8,6 @@ echo "Install libfuse to be able to use AppImages"
 sudo apt install libfuse2 -y
 echo ""
 
-echo "Remove that annoying Update Manager"
-sudo rm -f /usr/bin/update-manager
-echo ""
-
 echo "Lower wait time for refind"
 sudo sed -i "s/timeout 10/timeout 3/" /boot/EFI/refind/refind.conf
 echo ""
