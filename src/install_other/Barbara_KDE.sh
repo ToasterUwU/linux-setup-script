7aa39951f50851c0465ba2f1f9175f6cf7cecd51
ToasterUwU <aki@toasteruwu.com>
2024-03-07 16:38:00 +0100

    add all deps, just in case

--- src/install_other/Barbara_KDE.sh
+++ src/install_other/Barbara_KDE.sh
@@ -4,7 +4,7 @@ echo "Install Envision"
 sudo apt update
 sudo apt install -y libgtk-4-dev libadwaita-1-dev libssl-dev libjxl-dev libvte-2.91-gtk4-dev meson ninja-build git desktop-file-utils gettext file libusb-dev libusb-1.0-0-dev curl libeigen3-dev glslang-tools glslc
 
-sudo apt install libopenvr-dev libclang-dev libopenxr-dev -y
+sudo apt install -y build-essential pkg-config cmake libstdc++-12-dev libxkbcommon-dev fontconfig libfontconfig-dev libdbus-1-dev libasound2-dev python3 libopenvr-dev libopenvr-api1 libopenxr-dev libpipewire-0.3-0 libpipewire-0.3-dev libspa-0.2-dev clang
 git clone https://github.com/galister/wlx-overlay-s.git
 cd wlx-overlay-s || exit
 cargo install --path .
